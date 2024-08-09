f2667f560881b3e7aea8b1e2069f122c7089bbc3
fabioparis21 <parisfabio4@gmail.com>
2024-08-09 16:06:01 +0200

    Update and rename classificazione_multiclass.py to OneVSAll_multiclass.py
    Made the comments more meaningful and tidied up the code.

--- OneVSAll_multiclass.py
+++ OneVSAll_multiclass.py
@@ -1,6 +1,6 @@
 '''
-Modello di classificazione multiclasse con tecnica One vs All, utilizza più classificazioni binarie.
-Utilizzo dataset di numeri scritti a mano
+Multiclass classification with one vs all technique that uses multiple binary classifications.
+Hand written numbers dataset from sklearn
 '''
 
 import pandas as pd
@@ -9,74 +9,69 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss, accuracy_score
-#Dataset preset di sklearn
 from sklearn.datasets import load_digits
 
-#Carico dataset e creo arrays
 digits = load_digits()
 X = digits.data
 Y = digits.target
-#==> Il dataset ha 1797 esempi e 64 proprietà, ogni prop. rappresenta un pixel dell'immagine.
+#==>Dataset has 1797 examples with 64 features, every feature is a pixel.
 
-#Proviamo a visualizzare un'immagine partendo dalla sua matrice di pixel
-
+#Cycle to visualise the pixel matrix
 for i in range(0,10):
     pic_matrix = X[Y==i][0].reshape([8,8])
     plt.imshow(pic_matrix, cmap="gray")
     plt.show()
 
-    
+#Data preparation phase
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.3, random_state=0)
-#Essendo delle immagini conviene normalizzare tutti i dati per facilitare addestramento
+
 from sklearn.preprocessing import MinMaxScaler
 mms = MinMaxScaler()
 X_train = mms.fit_transform(X_train)
 X_test = mms.transform(X_test)
 
-#Passiamo ora alla classificazione multiclasse
-#LogisticRegression riconosce automaticamente il multiclasse e applica la tecnica OneVSAll
+#Modeling phase
+#LogisticRegression method automatically recognizes the multiclass method and applies OneVSAll technique
 lr = LogisticRegression()
 lr.fit(X_train, Y_train)
 Y_pred = lr.predict(X_test)
 Y_pred_proba = lr.predict_proba(X_test)
 
-#Calcolo metriche
+#Scoring phase
 print("ACCURACY= "+str(accuracy_score(Y_test, Y_pred)))
 print("LOSS= "+str(log_loss(Y_test, Y_pred_proba)))
 
-#Introduciamo nuova metrica, matrice di confusione, per vedere su quali classi il modello ha fatto più errori
+#We use the confusion matrix to see which classes the model failed to classify the most 
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(Y_test, Y_pred)
 
 #Passiamo alla visualizzazione della matrice generata
 import seaborn as sns
 '''
-Utilizzo una heatmap che indica per ciascuna classe quali e quanti errori ha fatto
-Es. il valore 2 nella settima riga e quarta colonna, ci dice che 2 immagini rappresentanti un 8 son state
-classificate come un 5! (conteggio parte sempre da zero)
+We use an heatmap that visualize for each class which and how much errors the model has done
 '''
 plt.figure(figsize=(9,9))
 sns.heatmap(cm, annot=True, cmap="Blues_r", linewidths=.5, square=True)
-plt.ylabel("Classe corrente")
-plt.xlabel("Classe predetta")
+plt.ylabel("Current class")
+plt.xlabel("Predicted class")
 
-#Sklearn implementa anche un altro modello di reg. multiclasse
+#Sklearn also has another type of OneVSAll classifier
 from sklearn.multiclass import OneVsRestClassifier
-#Quando lo si istanzia bisogna fornire un classificatore di base da utilizzare
+#The method requires a basic classifier to use
 ovr = OneVsRestClassifier(LogisticRegression())
-#Dopodiché si utilizza come un modello qualunque
+
 ovr.fit(X_train,Y_train)
 Y_pred1 = ovr.predict(X_test)
 Y_pred1_proba = ovr.predict_proba(X_test)
-#Metriche
+#Metrics
 print("ACCURACY= "+str(accuracy_score(Y_test, Y_pred1)))
 print("LOSS= "+str(log_loss(Y_test, Y_pred1_proba)))
 cm1 = confusion_matrix(Y_test, Y_pred1)
-#Matrice di confusione
+#Confusion matrix
 plt.figure(figsize=(9,9))
 sns.heatmap(cm1, annot=True, cmap="Blues_r", linewidths=.5, square=True)
-plt.ylabel("Classe corrente")
-plt.xlabel("Classe predetta")
+plt.ylabel("Current class")
+plt.xlabel("Predicted class")
 
 
 
